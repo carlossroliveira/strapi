b8fd7509bbf38007b5676e31f738fa2b04e837f3
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-03-13 16:56:33 -0300

    Feat: Strapi

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,1 @@
-export default () => ({
-  upload: {
-    config: {
-      breakpoints: {
-        large: 1000,
-        medium: 750,
-        small: 500,
-        thumbnail: 64,
-      },
-    },
-  },
-});
+export default () => ({});
